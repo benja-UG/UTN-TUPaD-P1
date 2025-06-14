0aff84fb43ccc36608e0211e91fcaedd459b23f5
Benjamin Ugarte <benjamintobiasugarte2003@gmail.com>
2025-06-14 19:37:20 -0300

    carpeta agregada

--- 03 Estructuras Condicionales/BENJAMIN-UGARTE_TP-03.py
+++ 03 Estructuras Condicionales/BENJAMIN-UGARTE_TP-03.py
@@ -94,4 +94,69 @@ elif media < mediana and mediana < moda:
     print("Sesgo negativo a la izquierda")
 else:
     print("Sin sesgo")
+"""""""""
+#7) Escribir un programa que solicite una frase o palabra al usuario. Si el string ingresado
+#termina con vocal, añadir un signo de exclamación al final e imprimir el string resultante por
+#pantalla; en caso contrario, dejar el string tal cual lo ingresó el usuario e imprimirlo por
+#pantalla.
+"""""
+frase = input("Ingresa una frese o palabra: ")
+vocal = frase [-1]
+
+if vocal in "aeiou":
+    frase += "!"
+    print(frase)
+else: 
+    print (frase)
+    
 """
+
+#8) Escribir un programa que solicite al usuario que ingrese su nombre y el número 1, 2 o 3
+#dependiendo de la opción que desee:
+#1. Si quiere su nombre en mayúsculas. Por ejemplo: PEDRO.
+#2. Si quiere su nombre en minúsculas. Por ejemplo: pedro.
+#3. Si quiere su nombre con la primera letra mayúscula. Por ejemplo: Pedro.
+#El programa debe transformar el nombre ingresado de acuerdo a la opción seleccionada por el
+#usuario e imprimir el resultado por pantalla. Nota: investigue uso de las funciones upper(),
+#lower() y title() de Python para convertir entre mayúsculas y minúsculas.
+"""""
+nombre = input("ingrese su nombre: ")
+print("y elija algunas de las siguientes opciones:")
+
+opciones = int(input("1. Nombre en mayúsculas \n2. Nombre en minúsculas \n3. Nombre con la primera letra mayúscula \nOpcion: "))
+
+if opciones == 1:
+    print(nombre.upper())
+elif opciones == 2:
+    print(nombre.lower())
+elif opciones == 3:
+    print (nombre.title())
+else:
+    print("Error, ingrese el numero 1, 2 o 3.")
+"""
+#9) Escribir un programa que pida al usuario la magnitud de un terremoto, clasifique la
+#magnitud en una de las siguientes categorías según la escala de Richter e imprima el resultado
+#por pantalla:
+#● Menor que 3: "Muy leve" (imperceptible).
+#● Mayor o igual que 3 y menor que 4: "Leve" (ligeramente perceptible).
+#● Mayor o igual que 4 y menor que 5: "Moderado" (sentido por personas, pero
+#generalmente no causa daños)
+#● Mayor o igual que 5 y menor que 6: "Fuerte" (puede causar daños en estructuras
+#débiles).
+#● Mayor o igual que 6 y menor que 7: "Muy Fuerte" (puede causar daños significativos).
+#● Mayor o igual que 7: "Extremo" (puede causar graves daños a gran escala).
+
+magnitud = int(input("Ingrese el numero de la magnitud de un terremoto: "))
+
+if magnitud < 3:
+    print(" Muy leve \n(imperceptible)")
+elif magnitud >= 3 and magnitud < 4:
+   print(" Leve \n(ligeramente perceptible)") 
+elif magnitud >= 4 and magnitud < 5:
+    print(" Moderado \n(sentido por personas, pero generalmente no causa daños)")
+elif  magnitud >= 5 and magnitud < 6:
+    print(" Fuerte \n(puede causar daños en estructuras débiles)")
+elif  magnitud >= 6 and magnitud < 7:
+    print(" Muy Fuerte \n(puede causar daños significativos)")
+elif  magnitud >= 7:
+    print(" Extremo \n(puede causar graves daños a gran escala)")
